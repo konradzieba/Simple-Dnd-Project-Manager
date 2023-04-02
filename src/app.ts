8b3ab8a41d452665e7a32d8f6772216506b2fd81
Konrad Zięba <162624@student.uwm.edu.pl>
2023-04-02 21:17:32 +0200

    add filter for project status

--- src/app.ts
+++ src/app.ts
@@ -132,7 +132,15 @@ class ProjectList {
 		this.element.id = `${type}-projects`;
 
 		projectState.addListener((projects: Project[]) => {
-			this.assignedProjects = projects;
+			const relevantProjects = projects.filter((project) => {
+				{
+					if (this.type === 'active') {
+						return project.status === ProjectStatus.Active;
+					}
+					return project.status === ProjectStatus.Finished;
+				}
+			});
+			this.assignedProjects = relevantProjects;
 			this.renderProjects();
 		});
 
